8b308c7e7194f278886bfdf0fa8908441442db83
kisarasandes1122 <kisarasandes22@gmail.com>
2024-03-24 13:06:08 +0530

    Update the UserDTO.java

--- src/main/java/com/Goappoint/GoAppoint/DTO/UserDTO.java
+++ src/main/java/com/Goappoint/GoAppoint/DTO/UserDTO.java
@@ -1,5 +1,6 @@
 package com.Goappoint.GoAppoint.DTO;
 
+import com.Goappoint.GoAppoint.Entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,4 +21,19 @@ public class UserDTO {
     private String district;
     private String zip;
     private String password;
+
+    public static UserDTO fromEntity(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUserID(user.getUserID());
+        userDTO.setUserFname(user.getUserFname());
+        userDTO.setUserLname(user.getUserLname());
+        userDTO.setUserEmail(user.getUserEmail());
+        userDTO.setUserMobile(user.getUserMobile());
+        userDTO.setUserAddress(user.getUserAddress());
+        userDTO.setCity(user.getCity());
+        userDTO.setDistrict(user.getDistrict());
+        userDTO.setZip(user.getZip());
+        userDTO.setPassword(user.getPassword());
+        return userDTO;
+    }
 }
